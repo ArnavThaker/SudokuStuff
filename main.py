0c58e8cea9d0d3d915549499f44e3e81414f707c
Arnav Thaker <arnav.thaker@gmail.com>
2022-07-28 14:09:48 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,48 @@
+import numpy as np
+
+
+class Cell:
+    def __init__(self, val, row, col, box):
+        """
+        Constructor for a single Sudoku cell
+
+        :param val: The current value of the cell
+        :param row: The row the cell is in (unchangeable after init)
+        :param col: The column the cell is in (unchangeable after init)
+        :param box: The box the cell is in (unchangable after init)
+        """
+        self.val = val
+        self.row = row
+        self.col = col
+        self.box = box
+
+    def __str__(self):
+        return "Value is {}, row is {}, col is {}, box is {}".format(self.val, self.row, self.col, self.box)
+
+
+class Board:
+    def __init__(self):
+        cells = []
+        """
+        Constructor for a Sudoku board. Cells are given proper meta values and initialized to 0
+        
+        """
+        for i in range(9):
+            for j in range(9):
+                new_cell = Cell(0, i, j, (j//3)+(i//3)*3)
+                cells.append(new_cell)
+        np_cells = np.array(cells)
+        np_cells.reshape(9, 9)
+        for cell in np_cells:
+            print(cell)
+
+
+    #def populate_board(self):
+
+
+
+def test():
+    board = Board()
+
+if __name__ == '__main__':
+    test()
