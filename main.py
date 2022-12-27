411b2e032f1a3bc7e1ccf13dc1700bfcff2b5d6c
Arnav Thaker <arnav.thaker@gmail.com>
2022-12-26 19:40:41 -0500

    Attempting a whole new overhaul of the design of the Sudoku board. Adding row, column, and box data structures seems redundant if cells already know what row, column, and box they are in. Perhaps OOP inheritence is an unneccesary/ extra step in this project that will overcomplicate it. In the event of a complete mess, restore this commit.

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import numpy as np
-
+import random
 
 class Cell:
     def __init__(self, val, row, col, box):
@@ -16,13 +16,11 @@ class Cell:
         self.col = col
         self.box = box
 
-    #def __str__(self):
-     #   return "{}{}{}".format(self.row, self.col, self.box)
-
     def __str__(self):
         return "{}".format(self.val)
 
     __repr__ = __str__
+
 
 class Row:
     def __init__(self):
@@ -36,6 +34,7 @@ class Row:
 
     __repr__ = __str__
 
+
 class Column(Row):
     pass
 
@@ -43,89 +42,91 @@ class Column(Row):
 class Box(Row):
     pass
 
+
 class Board:
     def __init__(self):
         """
         Constructor for a Sudoku board. Cells are given proper meta values and initialized to 0
         
         """
-        self.init_board()
-
-
-    def init_board(self):
         cells = []
         for i in range(9):
             for j in range(9):
-                new_cell = Cell(i, i, j, (j//3)+(i//3)*3) # change value back to 0
+                new_cell = Cell(' ', i, j, (j // 3) + (i // 3) * 3)  # change value back to 0
                 cells.append(new_cell)
-        board = np.array(cells)
-        board = board.reshape(9, 9)
-        return board
+        self.board = np.array(cells)
+        self.board = self.board.reshape(9, 9)
+        print(self.board)
+        self.rows = self.create_rows()
+        self.cols = self.create_cols()
+        self.boxes = self.create_boxes()
+
 
     def create_rows(self):
         rows = []
-        board = self.init_board()
-        print(board)
-        for i in board:
+        for i in self.board:
             current_row = Row()
             for j in i:
                 current_row.add_cell(j)
             rows.append(current_row)
-        rows = np.array(rows)
-        rows = rows.reshape(9, 1)
-        print(rows)
         # subscript family with object[row][0].cells[col]
         return rows
 
     def create_cols(self):
         cols = []
-        board = self.init_board()
         for i in range(9):
             current_col = Column()
-            for j in board[0:9, i]:
+            for j in self.board[0:9, i]:
                 current_col.add_cell(j)
             cols.append(current_col)
-        cols = np.array(cols)
-        cols = cols.reshape(9, 1)
-        print(cols)
         return cols
 
     def create_boxes(self):
         boxes = []
-        board = self.init_board()
         for i in range(3):
             current_box = Box()
-            for j in board[(i//3)*3:(i//3)*3+3, i*3:i*3+3]:
+            for j in self.board[(i//3)*3:(i//3)*3+3, i*3:i*3+3]:
                 for k in j:
                     current_box.add_cell(k)
             boxes.append(current_box)
         for i in range(3, 6):
             current_box = Box()
-            for j in board[(i//3)*3:(i//3)*3+3, (i-3)*3:(i-3)*3+3]:
+            for j in self.board[(i//3)*3:(i//3)*3+3, (i-3)*3:(i-3)*3+3]:
                 for k in j:
                     current_box.add_cell(k)
             boxes.append(current_box)
         for i in range(6, 9):
             current_box = Box()
-            for j in board[(i//3)*3:(i//3)*3+3, (i-6)*3:(i-6)*3+3]:
+            for j in self.board[(i//3)*3:(i//3)*3+3, (i-6)*3:(i-6)*3+3]:
                 for k in j:
                     current_box.add_cell(k)
             boxes.append(current_box)
-        boxes = np.array(boxes)
-        boxes = boxes.reshape(9, 1)
-        print(boxes)
         return boxes
 
+    def populate_board(self):
+        for row in self.rows:
+            digits = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+            for cell in row.cells:
+                val = random.choice(digits)
+                digits.remove(val)
+                if self.check(val, row.cells) and self.check(val, self.cols[cell.col].cells) and self.check(val, self.boxes[cell.box].cells):
+                    cell.val = val
+                else:
+                    cell.val = 'changed'
 
-
-
+    @staticmethod
+    def check(val, obj):
+        for cell in obj:
+            if cell.val == val:
+                return False
+            else:
+                return True
 
 
 def test():
     board = Board()
-    board.create_rows()
-    board.create_cols()
-    board.create_boxes()
+    board.populate_board()
+    print(board.board)
 
 
 if __name__ == '__main__':
