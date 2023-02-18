4c09ecbde4098e0b4cb4a438fb0a6cae3ef66334
Arnav Thaker <arnav.thaker@gmail.com>
2023-02-17 19:57:01 -0500

    Switched from tkinter to pyqt. Tkinter is pretty whack. Will start implementing real stuff soon

--- gui.py
+++ gui.py
@@ -1,7 +1,9 @@
-from tkinter import *
+import sys
+from PyQt6.QtWidgets import QApplication, QLabel, QWidget
 
-root = Tk()
-root.title("Testing")
-root.geometry("210x180")
-
-root.mainloop()
+app = QApplication([])
+window = QWidget()
+window.setWindowTitle("Sudoku")
+window.setGeometry(100, 100, 280, 80) # x start, y start, width, height
+helloMsg = QLabel("<h1>Hello, World!</h1>", parent=window) # This is in HTML, which I have 0 clue how to use
+helloMsg.move(60, 15)
